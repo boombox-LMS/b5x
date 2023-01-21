08f4b9d8883f2eb0f32aa24286b342fd9c98cb18
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-21 14:07:32 -0600

    Sketch in simple copy + paste dropzone

--- packages/viewer/src/features/publishing/TopicPublisher.js
+++ packages/viewer/src/features/publishing/TopicPublisher.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setHeaderProps } from "../header/headerSlice";
 
@@ -16,5 +16,38 @@ export const TopicPublisher = () => {
     );
   }, []);
 
-  return <div style={{ padding: "25px 40px" }}>TopicPublisher</div>;
+  const [dropzoneIsActive, setDropzoneIsActive] = useState(false);
+
+  let dropzoneStyle = {
+    width: "100%",
+    height: "100px",
+    border: "2px dashed #999",
+    borderRadius: "5px",
+    lineHeight: "100px",
+    textAlign: "center",
+  };
+
+  if (dropzoneIsActive) {
+    dropzoneStyle.borderColor = "#20A7F5";
+  }
+
+  return (
+    <div style={{ padding: "25px 40px" }}>
+      <h1>Publish a topic</h1>
+      <div
+        style={dropzoneStyle}
+        onMouseEnter={() => setDropzoneIsActive(true)}
+        onMouseLeave={() => setDropzoneIsActive(false)}
+      >
+        {dropzoneIsActive && (
+          <span style={{ color: "#20A7F5" }}>
+            Listening for pasted topic data ...
+          </span>
+        )}
+        {!dropzoneIsActive && (
+          <span>Hover over me, then paste your topic data</span>
+        )}
+      </div>
+    </div>
+  );
 };
