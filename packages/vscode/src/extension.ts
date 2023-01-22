4bb7c1b6b4d6c37969b92d5c3448730327336e04
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-22 13:46:13 -0600

    Refactor

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -2,14 +2,8 @@ import * as vscode from 'vscode';
 import { buildTopicConfig, createTopicFolder } from './topicCreation';
 import { listTopicFolders } from './fsHelpers';
 
-// Run this function again after new topics are created.
-// Looks like this could run when the user changes workspace folders
-// if we listen for the onDidChangeWorkspaceFolders event
-// available on vscode.workspace
-
 /**
- * Scan the workspace for topic folders,
- * and return an array of folder URIs
+ * Return an array of folder URIs
  * representing all of the topic folders in the workspace.
  */
 function scanWorkspaceForTopicFolders() {
@@ -28,14 +22,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	let createNewTopic = vscode.commands.registerCommand('b5x.createNewTopic', (resourceUri) => {
-		/*
-		vscode.window.showInputBox({prompt: "Enter a topic identifier to use in the topic's URL (e.g., my-new-topic):"}).then((value) => {
-			vscode.window.showInformationMessage(`Entered value: ${value}`);
-		});
-		*/
-		console.log('resourceUri: ' + resourceUri);
 		buildTopicConfig();
-		createTopicFolder(resourceUri);
+		createTopicFolder(resourceUri.fsPath);
 		scanWorkspaceForTopicFolders();
 		vscode.window.showInformationMessage('Topic folder created.');
 	});
--- packages/vscode/src/topicCreation.ts
+++ packages/vscode/src/topicCreation.ts
@@ -1,12 +1,15 @@
 import * as vscode from 'vscode';
 
 export function buildTopicConfig() {
-	// query the user for the topic parameters
-	// return a topic config object
+  /*
+  vscode.window.showInputBox({prompt: "Enter a topic identifier to use in the topic's URL (e.g., my-new-topic):"}).then((value) => {
+    vscode.window.showInformationMessage(`Entered value: ${value}`);
+  });
+  */
 	console.log('buildTopicConfig');
 }
 
-export function createTopicFolder(parentFolderUri: vscode.Uri) {
+export function createTopicFolder(topicParentDir: string) {
 	// create a new topic folder that includes an images directory etc.
 	// add the topic folder to the list of topic folders that can be built
 	console.log('createTopicFolder');
