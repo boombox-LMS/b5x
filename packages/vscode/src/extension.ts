9c2055ea903f0ab7516c50ef272154422e7f7b99
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-22 13:27:24 -0600

    Make the create/build commands show up only in the correct context on initial VSCode load

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -3,15 +3,41 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 
-// run this function again after new topics are created
-function findTopicFolders() {
-	console.log('findTopicFolders');
+// Run this function again after new topics are created.
+// Looks like this could run when the user changes workspace folders
+// if we listen for the onDidChangeWorkspaceFolders event
+// available on vscode.workspace
+
+function listTopicFolders(dir: string) {
+	let results: string[] = [];
+	const list = fs.readdirSync(dir);
+	list.forEach(function(file) {
+			file = dir + '/' + file;
+			const stat = fs.statSync(file);
+			if (stat && stat.isDirectory()) { 
+				results = results.concat(listTopicFolders(file));
+			} else { 
+				let fileName = file.split(/(\\|\/)/g).pop();
+				if (fileName === 'topic-config.yaml') {
+					results.push(dir)
+				}
+			}
+	});
+	return [...new Set(results)];
+}
+
+/**
+ * Scan the workspace for topic folders,
+ * and return an array of folder URIs
+ * representing all of the topic folders in the workspace.
+ */
+function scanWorkspaceForTopicFolders() {
 	if (!vscode.workspace.workspaceFolders) {
 		return;
 	}
 	const rootFolderUri = vscode.workspace.workspaceFolders[0].uri;
-	// find all topic folders
-	// add them to the list of topic folders that can be built
+	const topicFolders = listTopicFolders(rootFolderUri.fsPath);
+	vscode.commands.executeCommand('setContext', 'b5x.topicFolders', topicFolders);
 }
 
 function buildTopicConfig() {
@@ -20,7 +46,7 @@ function buildTopicConfig() {
 	console.log('buildTopicConfig');
 }
 
-function createTopicFolder() {
+function createTopicFolder(parentFolderUri: vscode.Uri) {
 	// create a new topic folder that includes an images directory etc.
 	// add the topic folder to the list of topic folders that can be built
 	console.log('createTopicFolder');
@@ -30,23 +56,23 @@ function createTopicFolder() {
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log('activated');
-	findTopicFolders();
+	scanWorkspaceForTopicFolders();
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	let createNewTopic = vscode.commands.registerCommand('b5x.createNewTopic', ({ fsPath }) => {
+	let createNewTopic = vscode.commands.registerCommand('b5x.createNewTopic', (resourceUri) => {
 		/*
 		vscode.window.showInputBox({prompt: "Enter a topic identifier to use in the topic's URL (e.g., my-new-topic):"}).then((value) => {
 			vscode.window.showInformationMessage(`Entered value: ${value}`);
 		});
 		*/
-		// console.log(fs.readdirSync(fsPath));
+		console.log('resourceUri: ' + resourceUri);
 		buildTopicConfig();
-		createTopicFolder();
-		findTopicFolders();
+		createTopicFolder(resourceUri);
+		scanWorkspaceForTopicFolders();
 		vscode.window.showInformationMessage('Topic folder created.');
 	});
 
-	let buildTopic = vscode.commands.registerCommand('b5x.buildTopicData', () => {
+	let buildTopic = vscode.commands.registerCommand('b5x.buildTopicData', (resourceUri) => {
 		vscode.window.showInformationMessage('Build Topic Data');
 	});
 
