7c526172ce746a8e8c7e76bf3da4de7b858dadcf
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-22 14:36:18 -0600

    Add file utils needed to populate an example documents folder

--- packages/vscode/src/fsHelpers.ts
+++ packages/vscode/src/fsHelpers.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
 export function listTopicFolders(dir: string) {
 	let results: string[] = [];
@@ -17,3 +18,41 @@ export function listTopicFolders(dir: string) {
 	});
 	return [...new Set(results)];
 }
+
+// Thanks, Simon Zyx on Stack Overflow:
+// https://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
+export const copyFileSync = (source: string, target: string) => {
+  let targetFile = target;
+
+  // If target is a directory, a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+};
+
+export const copyFolderRecursiveSync = (source: string, target: string) => {
+  let files = [];
+
+  // Check if folder needs to be created or integrated
+  let targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  // Copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file: string) {
+      let curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
+};
---
+++ packages/vscode/src/templates/topic/documents/999-feedback.md
@@ -0,0 +1,11 @@
+# Leave feedback on this topic
+
+<nps-question id='b5x-topic-nps'>
+How likely are you to recommend this topic to a colleague?
+</nps-question>
+
+<long-text-question id='b5x-topic-nps-explanation'>
+Please explain your rating:
+</long-text-question>
+
+Thank you for your feedback!
