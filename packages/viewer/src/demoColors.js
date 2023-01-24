c1647329fb1ba8af9b3a1f0a075201f07a9de4de
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-23 22:54:57 -0600

    Rename file to suit expanded scope

--- packages/viewer/src/demoColors.js
+++
@@ -1,6 +0,0 @@
-// A .gitignore-d file that will override the UI colors if populated for a corporate demo
-
-export const demoColors = {
-  main: undefined,
-  secondary: undefined,
-};
---
+++ packages/viewer/src/demoVars.js
@@ -0,0 +1,7 @@
+// A .gitignore-d file that will override the UI colors if populated for a corporate demo
+
+export const demoVars = {
+  primaryColor: undefined,
+  secondaryColor: undefined,
+  headerLogoUrl: undefined,
+};
