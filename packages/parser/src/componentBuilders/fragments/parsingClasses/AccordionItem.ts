95c15922ea5ac7d12d0e5199ae0c47212c798941
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-28 20:54:05 -0600

    Add manifest for AccordionItem

--- packages/parser/src/componentBuilders/fragments/parsingClasses/AccordionItem.ts
+++ packages/parser/src/componentBuilders/fragments/parsingClasses/AccordionItem.ts
@@ -2,6 +2,13 @@ import { FragmentViaBxmlTag } from "../abstractClasses/FragmentViaBxmlTag";
 import { z } from "zod";
 import { FragmentViaBxmlTagParams } from "../../../types/fragments";
 import { BxmlTagNodeSchema, BxmlTextNodeSchema } from "../../../types/bxmlNodes";
+import { RawFragmentSchema } from "@b5x/types";
+
+/**
+ * The accordion item is created in the parsing of the accordion tag,
+ * and is not invoked directly in markup, so it does not have its own
+ * example markup.
+ */
 
 export const AccordionItemTagSchema = z
   .object({
@@ -13,6 +20,15 @@ export const AccordionItemTagSchema = z
   .strict();
 
 export type AccordionItemTag = z.infer<typeof AccordionItemTagSchema>;
+
+export const AccordionItemApiDataSchema = RawFragmentSchema.extend({
+  contentType: z.literal("AccordionItem"),
+  contents: z.literal(''),
+  isRequired: z.literal(false),
+  isStateful: z.literal(false),
+  childUris: z.array(z.string()).min(1),
+  data: z.object({ title: z.string() }).strict(),
+}).strict();
 
 /**
  * A fragment that renders as a collapsible accordion item.
@@ -26,3 +42,12 @@ export class AccordionItem extends FragmentViaBxmlTag {
     this.data = { title: this.bxmlNode.attribs.title };
   }
 }
+
+export const manifest = {
+  contentType: "AccordionItem",
+  tagName: "accordion-item",
+  exampleMarkupStrings: [],
+  parsingClass: AccordionItem,
+  tagSchema: AccordionItemTagSchema,
+  apiDataSchema: AccordionItemApiDataSchema,
+};
