aa9395dc8d16cba43c3ac20a52373d60fef261ae
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-01-28 20:38:44 -0600

    Update the test setup to parse fragments recursively, replacing shallow parsing

--- packages/parser/src/tests/setupBeforeEachTestFile.js
+++ packages/parser/src/tests/setupBeforeEachTestFile.js
@@ -12,16 +12,28 @@ global.buildFragmentFromMarkupString = (
   const markup = markupStr.trim();
   const domElement = htmlparser2.parseDocument(markup, { xmlMode: true });
   let bxmlNode = coerceToBxmlNode(domElement);
+
   // pop off automatically added root tag
   bxmlNode = bxmlNode.children[0];
+
+  // mock the document properties required by the fragment builder
   const mockDocument = {
     uri: "mock-document-uri",
     topic: { uri: "mock-topic-uri" },
   };
+  
+  // recursively build the fragment
+  return buildFragment(uri, bxmlNode, mockDocument);
+};
+
+function buildFragment(uri, bxmlNode, document) {
   const fragment = new FragmentBuilder({
     uri,
     bxmlNode,
-    document: mockDocument,
+    document,
   }).build();
+  fragment.childBxmlNodes.forEach(childBxmlNode => {
+    fragment.childFragments.push(buildFragment(uri, childBxmlNode, document))
+  })
   return fragment;
-};
+}
