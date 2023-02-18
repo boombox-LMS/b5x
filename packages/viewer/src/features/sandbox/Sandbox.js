21a7f76362bf4913b2d4d9b29248a9a550ef6d92
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-02-18 16:27:57 -0600

    Sketch in sticky header portion

--- packages/viewer/src/features/sandbox/Sandbox.js
+++ packages/viewer/src/features/sandbox/Sandbox.js
@@ -1,38 +1,55 @@
 import * as React from "react";
 import styled from "styled-components/macro";
 
+const sidebarDrawerIsOpen = false;
+
 const Layout = () => {
   const layoutContainerCss = `
     height: 100%;
+    position: relative;
   `;
 
-  const mainContentAreaCss = `
-    outline: 2px solid orange;
-    padding: 10px;
-    padding-top: 30px;
+  let mainContentAreaCss = `
+    background-color: orange;
+    padding: 30px;
     display: inline-block;
     vertical-align: top;
-    width: calc(100% - 300px);
-    min-height: 95%;
+    min-height: 2000px;
+    width: 70%;
   `;
 
-  const sidebarDrawerCss = `
-    outline: 2px solid green;
+  let sidebarDrawerCss = `
+    background-color: green;
     width: 300px;
     display: inline-block;
     vertical-align: top;
     min-height: 95%;
+    position: relative;
   `;
 
-  const headerCss = `
-    outline: 2px solid blue;
+  const topHeaderCss = `
+    background-color: blue;
+    color: white;
+    height: 75px;
+    padding: 5px;
+  `;
+
+  const stickyHeaderCss = `
+    background-color: red;
+    color: white;
+    height: 35px;
+    padding: 5px;
+    position: sticky;
+    top: 0;
+    z-index: 1;
   `;
 
   return (
     <div css={layoutContainerCss}>
-      <div css={headerCss}>Header</div>
-      <div css={sidebarDrawerCss}>Sidebar drawer</div>
-      <div css={mainContentAreaCss}>Main content area</div>
+      <div css={topHeaderCss}>TOP HEADER PORTION</div>
+      <div css={stickyHeaderCss}>STICKY HEADER PORTION</div>
+      <div css={sidebarDrawerCss}>SIDEBAR DRAWER</div>
+      <div css={mainContentAreaCss}>MAIN CONTENT AREA</div>
     </div>
   );
 };
