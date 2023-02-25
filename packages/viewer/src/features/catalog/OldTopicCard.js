8e86a8a28203417c8ed4bb4f757f8329b480c838
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-02-24 19:10:20 -0600

    Make the topic thumbnail its own component

--- packages/viewer/src/features/catalog/OldTopicCard.js
+++ packages/viewer/src/features/catalog/OldTopicCard.js
@@ -11,7 +11,89 @@ import {
   BuildOutlined,
   FileTextOutlined,
 } from "@ant-design/icons";
-import { TopicThumbnail } from "./TopicThumbnail";
+import GeoPattern from "geopattern";
+import tinycolor from "tinycolor2";
+
+const combineTwoShortestLines = (lines) => {
+  let indexToCombineWithNextSibling = 0;
+  let combinedLine = lines[0] + " " + lines[1];
+  for (let i = 0; i < lines.length - 1; i++) {
+    const thisCombinedLine = lines[i] + " " + lines[i + 1];
+    if (thisCombinedLine.length < combinedLine.length) {
+      combinedLine = thisCombinedLine;
+      indexToCombineWithNextSibling = i;
+    }
+  }
+  lines[indexToCombineWithNextSibling] = combinedLine;
+  lines.splice(indexToCombineWithNextSibling + 1, 1);
+  return lines;
+};
+
+const splitIntoThreeLines = (str) => {
+  // split into words
+  let lines = str.split(" ");
+  while (lines.length > 3) {
+    lines = combineTwoShortestLines(lines);
+  }
+  return lines;
+};
+
+const GeneratedTopicImage = ({ topic }) => {
+  let title = topic.title;
+  const geopattern = GeoPattern.generate(title);
+  const titleColor = tinycolor(geopattern.color).lighten(30).toString();
+
+  // get the max line length when the title is split into 3 lines
+  const lines = splitIntoThreeLines(title);
+  let maxLineLength;
+  if (lines.length === 1) {
+    maxLineLength = lines[0].length;
+  } else {
+    maxLineLength = lines.reduce((a, b) => {
+      return a.length > b.length ? a.length : b.length;
+    });
+  }
+
+  // the minimum font size
+  let fontSize = 24;
+
+  // don't attempt to display the title at all if it's too long
+  if (maxLineLength > 11) {
+    title = "";
+    // otherwise, bump up the font size by max line length
+  } else {
+    fontSize = fontSize + 3.6 * (11 - maxLineLength);
+  }
+
+  // cap the font size for 3-line titles to ensure vertical fit
+  if (lines.length === 3) {
+    if (fontSize > 36) {
+      fontSize = 36;
+    }
+  }
+
+  const style = {
+    height: "125px",
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    flexDirection: "column",
+    whiteSpace: "no-wrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    borderRadius: "5px 5px 0px 0px",
+    textAlign: "center",
+    width: "250px",
+    backgroundImage: geopattern.toDataUrl(),
+    fontFamily: "Bungee Shade",
+    color: titleColor,
+    fontSize: `${fontSize}px`,
+    lineHeight: "0.85em",
+    padding: "10px",
+  };
+
+  return <div style={style}>{title}</div>;
+};
 
 export const TopicCard = ({ topic }) => {
   const navigate = useNavigate();
@@ -86,7 +168,10 @@ export const TopicCard = ({ topic }) => {
           </div>
         </>
       )}
-      <TopicThumbnail topic={topic} />
+      {topic.coverImageUrl !== "" && (
+        <img className="topic-card__cover-image" src={topic.coverImageUrl} />
+      )}
+      {topic.coverImageUrl === "" && <GeneratedTopicImage topic={topic} />}
       <div className="topic-card__info">
         <div className="topic-card__title">
           {topic.unmetPrerequisites.length > 0 && (
---
+++ packages/viewer/src/features/catalog/TopicThumbnail.js
@@ -0,0 +1,94 @@
+import React from "react";
+import GeoPattern from "geopattern";
+import tinycolor from "tinycolor2";
+
+const combineTwoShortestLines = (lines) => {
+  let indexToCombineWithNextSibling = 0;
+  let combinedLine = lines[0] + " " + lines[1];
+  for (let i = 0; i < lines.length - 1; i++) {
+    const thisCombinedLine = lines[i] + " " + lines[i + 1];
+    if (thisCombinedLine.length < combinedLine.length) {
+      combinedLine = thisCombinedLine;
+      indexToCombineWithNextSibling = i;
+    }
+  }
+  lines[indexToCombineWithNextSibling] = combinedLine;
+  lines.splice(indexToCombineWithNextSibling + 1, 1);
+  return lines;
+};
+
+const splitIntoThreeLines = (str) => {
+  // split into words
+  let lines = str.split(" ");
+  while (lines.length > 3) {
+    lines = combineTwoShortestLines(lines);
+  }
+  return lines;
+};
+
+const GeneratedTopicImage = ({ topic }) => {
+  let title = topic.title;
+  const geopattern = GeoPattern.generate(title);
+  const titleColor = tinycolor(geopattern.color).lighten(30).toString();
+
+  // get the max line length when the title is split into 3 lines
+  const lines = splitIntoThreeLines(title);
+  let maxLineLength;
+  if (lines.length === 1) {
+    maxLineLength = lines[0].length;
+  } else {
+    maxLineLength = lines.reduce((a, b) => {
+      return a.length > b.length ? a.length : b.length;
+    });
+  }
+
+  // the minimum font size
+  let fontSize = 24;
+
+  // don't attempt to display the title at all if it's too long
+  if (maxLineLength > 11) {
+    title = "";
+    // otherwise, bump up the font size by max line length
+  } else {
+    fontSize = fontSize + 3.6 * (11 - maxLineLength);
+  }
+
+  // cap the font size for 3-line titles to ensure vertical fit
+  if (lines.length === 3) {
+    if (fontSize > 36) {
+      fontSize = 36;
+    }
+  }
+
+  const style = {
+    height: "125px",
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    flexDirection: "column",
+    whiteSpace: "no-wrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    borderRadius: "5px 5px 0px 0px",
+    textAlign: "center",
+    width: "250px",
+    backgroundImage: geopattern.toDataUrl(),
+    fontFamily: "Bungee Shade",
+    color: titleColor,
+    fontSize: `${fontSize}px`,
+    lineHeight: "0.85em",
+    padding: "10px",
+  };
+
+  return <div style={style}>{title}</div>;
+};
+
+export const TopicThumbnail = ({ topic }) => {
+  if (topic.coverImageUrl) {
+    return (
+      <img className="topic-card__cover-image" src={topic.coverImageUrl} />
+    );
+  } else {
+    return <GeneratedTopicImage topic={topic} />;
+  }
+};
