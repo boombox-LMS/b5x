6b8a9f2c19b065c7799a323a8a5815a83e078031
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-02-25 00:13:04 -0600

    Tweak topic card grid

--- packages/viewer/src/features/catalog/TopicList.js
+++ packages/viewer/src/features/catalog/TopicList.js
@@ -4,8 +4,8 @@ import styled from "styled-components/macro";
 
 const TopicGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(225px, 1fr));
-  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 22px;
   margin: 15px;
 `;
 
