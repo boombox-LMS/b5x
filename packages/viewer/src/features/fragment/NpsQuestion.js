d1e91c8b892399db3ad9560d8c75219c2f7a58c1
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-02-25 16:30:55 -0600

    Deprecate NPS legend CSS

--- packages/viewer/src/features/fragment/NpsQuestion.js
+++ packages/viewer/src/features/fragment/NpsQuestion.js
@@ -1,5 +1,60 @@
 import React, { useState } from "react";
 import { FragmentWrapper } from "./FragmentWrapper";
+import styled from "styled-components/macro";
+
+/*
+.nps-question__selector {
+  display: grid;
+  margin-top: 12px;
+  margin-bottom: 4px;
+  grid-template-columns: repeat(11, 1fr);
+}
+
+.nps-question__option {
+  border: 1.5px solid #e0e0e0;
+  text-align: center;
+  padding: 5px;
+  margin-right: -1px;
+}
+
+.nps-question__option:last-of-type {
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+
+.nps-question__option:first-of-type {
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+
+.nps-question__option.nps-question__option--selected {
+  background-color: var(--completed-light);
+  border: 1.5px solid var(--completed-dark);
+  color: var(--completed-dark);
+  font-weight: bold;
+  margin-right: 0px;
+}
+
+.nps-question__option--selected + .nps-question__option {
+  margin-left: -1px;
+  z-index: -1;
+}
+*/
+
+const LegendLeftItem = styled.div`
+  text-align: left;
+`;
+
+const LegendRightItem = styled.div`
+  text-align: right;
+`;
+
+const Legend = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  font-size: 0.9em;
+  font-style: italic;
+`;
 
 export const NpsQuestion = ({ fragment, response, responseUpdateCallback }) => {
   let responseValue;
@@ -44,10 +99,10 @@ export const NpsQuestion = ({ fragment, response, responseUpdateCallback }) => {
           </div>
         ))}
       </div>
-      <div className="nps-question__legend">
-        <div className="nps-question__legend--left">Not at all likely</div>
-        <div className="nps-question__legend--right">Extremely likely</div>
-      </div>
+      <Legend>
+        <LegendLeftItem>Not at all likely</LegendLeftItem>
+        <LegendRightItem>Extremely likely</LegendRightItem>
+      </Legend>
     </div>
   );
 };
