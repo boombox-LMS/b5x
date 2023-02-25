2238d385a6f546e934533240e364b971dae7a894
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-02-25 11:57:21 -0600

    Deprecate CodeBlock CSS

--- packages/viewer/src/features/fragment/CodeBlock.js
+++ packages/viewer/src/features/fragment/CodeBlock.js
@@ -4,6 +4,44 @@ import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { CopyOutlined, CheckOutlined } from "@ant-design/icons";
 import he from "he";
+import styled from "styled-components/macro";
+
+/*
+.code-block .text-copier .text-copier__flash {
+  position: absolute;
+  width: 300px;
+  text-align: right;
+  top: -33px;
+  right: 0px;
+  font-size: 0.9em;
+  font-style: italic;
+  color: var(--pop);
+  transition: 0.4s;
+}
+
+.text-copier__flash--active {
+  opacity: 100%;
+}
+
+.text-copier__flash--inactive {
+  opacity: 0%;
+}
+
+*/
+
+const TextCopierWrapper = styled.div`
+  position: absolute;
+  right: 3px;
+  top: 6px;
+`;
+
+const TextCopierIconWrapper = styled.div`
+  color: white;
+  font-size: 1.2em;
+  padding: 0px 3px;
+  border-radius: 2px;
+  opacity: 70%;
+`;
 
 export const CodeBlock = ({ fragment }) => {
   const [textToCopy, setTextToCopy] = useState(fragment.contents);
@@ -18,8 +56,13 @@ export const CodeBlock = ({ fragment }) => {
   }, [copied]);
 
   return (
-    <div className="code-block">
-      <div className="text-copier">
+    <div
+      css={`
+        position: relative;
+      `}
+      className="code-block"
+    >
+      <TextCopierWrapper className="text-copier">
         <div
           className={`text-copier__flash ${
             copied
@@ -30,11 +73,11 @@ export const CodeBlock = ({ fragment }) => {
           copied to clipboard
         </div>
         <CopyToClipboard onCopy={() => setCopied(true)} text={textToCopy}>
-          <div className="text-copier__icon" onClick={() => setCopied(true)}>
+          <TextCopierIconWrapper onClick={() => setCopied(true)}>
             <CopyOutlined />
-          </div>
+          </TextCopierIconWrapper>
         </CopyToClipboard>
-      </div>
+      </TextCopierWrapper>
       <div className="code-block__contents">
         <SyntaxHighlighter
           language={fragment.data.language}
